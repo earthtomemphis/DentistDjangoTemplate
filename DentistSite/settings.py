8a26dbf1f1a6e918083a5c6f5bfce7058bb347d2
Memphis DiAngelis <earthtomemphis@gmail.com>
2020-11-11 10:10:57 -0700

    instaled pyscopg2

--- DentistSite/settings.py
+++ DentistSite/settings.py
@@ -129,8 +129,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'earthtomemphis@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
